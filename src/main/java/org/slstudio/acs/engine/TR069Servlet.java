948d714d4b41032a1f435ad929c7e69f71986174
Chandler Li <swimlurker@gmail.com>
2013-04-23 01:32:24 +0800

    initial pom file

---
+++ src/main/java/org/slstudio/acs/engine/TR069Servlet.java
@@ -0,0 +1,22 @@
+package org.slstudio.acs.engine;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: chandler
+ * Date: 13-4-23
+ * Time: ÉÏÎç12:47
+ * To change this template use File | Settings | File Templates.
+ */
+public class TR069Servlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getOutputStream().write("Hello".getBytes());
+    }
+}
