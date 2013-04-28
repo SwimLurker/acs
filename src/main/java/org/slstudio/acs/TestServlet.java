0812fe4f3fbf6d3d35e5add68959880a9ab0aff1
Chandler Li <swimlurker@gmail.com>
2013-04-29 01:02:43 +0800

    add some src code

--- src/main/java/org/slstudio/acs/TestServlet.java
+++ src/main/java/org/slstudio/acs/TestServlet.java
@@ -4,6 +4,8 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.PrintWriter;
 
@@ -17,9 +19,18 @@ public class TestServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String rootPath = req.getSession().getServletContext().getRealPath("");
+        String fileName = rootPath + File.separator +"WEB-INF"+File.separator+"classes"+File.separator+"log4j.properties";
+        FileReader fr = new FileReader(fileName);
+        int c = -1;
+        StringBuffer sb = new StringBuffer();
+        while ((c = fr.read())!= -1){
+           sb.append(c);
+        }
+        fr.close();
+
         String responseStr = "";
         responseStr += "<html><body>";
-        responseStr += rootPath;
+        responseStr += sb.toString();
         responseStr += "</body></html>";
         resp.setStatus(HttpServletResponse.SC_OK);
         resp.setContentType("text/html");
